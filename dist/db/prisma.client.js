eb8185a2ff17f3887746387da9303fe74ee47648
Wisdom <eluewisdom@gmail.com>
2023-06-13 22:38:50 +0100

    cleanup: code cleanup

--- dist/db/prisma.client.js
+++ dist/db/prisma.client.js
@@ -1,8 +1,5 @@
 "use strict";
-// import { PrismaClient } from "./prisma/@prisma/client";
-// const prisma = new PrismaClient();
 Object.defineProperty(exports, "__esModule", { value: true });
-// export default prisma;
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 exports.default = prisma;
--- server/db/prisma.client.ts
+++ server/db/prisma.client.ts
@@ -1,8 +1,3 @@
-// import { PrismaClient } from "./prisma/@prisma/client";
-// const prisma = new PrismaClient();
-
-// export default prisma;
-
 import { PrismaClient } from "@prisma/client";
 
 const prisma: PrismaClient = new PrismaClient();
