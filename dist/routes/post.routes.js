6716f8de8e4ddcb07e19428ceedf116bfff7d116
Wisdom <eluewisdom@gmail.com>
2023-08-05 23:36:20 +0100

    fix: wrong param name passed to post route

--- dist/routes/post.routes.js
+++ dist/routes/post.routes.js
@@ -5,5 +5,5 @@ const post_controller_1 = require("../controllers/post.controller");
 const auth_middleware_1 = require("../middleware/auth.middleware");
 const router = (0, express_1.Router)();
 router.route("/").get(post_controller_1.getPosts).post(auth_middleware_1.verifyAuth, post_controller_1.addPost);
-router.route("/:slug/:id").get(post_controller_1.getSinglePost).put(auth_middleware_1.verifyAuth, post_controller_1.updatePost);
+router.route("/:slug/:postId").get(post_controller_1.getSinglePost).put(auth_middleware_1.verifyAuth, post_controller_1.updatePost);
 exports.default = router;
